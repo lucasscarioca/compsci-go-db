af1b1414f88c3214754739907572bb77e08aecc5
Lucas Carioca <lucascarioca97@gmail.com>
2023-09-06 00:25:38 -0300

    feat: finished hashIndex; testing

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
 
+	"github.com/lucasscarioca/custom-db/db"
+)
+
+func main() {
+	file, err := os.Open("./data/words_test.txt")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		lines = append(lines, line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+
+	db.NewHashIndex(50, lines)
 }
